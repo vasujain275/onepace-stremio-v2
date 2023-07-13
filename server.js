6bdf52487fe0aacf08ebd8868714d7da5ed2fb59
Vasu <vasujain275@gmail.com>
2023-07-13 23:55:11 +0530

    Completed addon for v1.0.0 and deployed

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ addon.addonBuilder().then((result)=>{
     serveHTTP(result, { port:  process.env.PORT || 3000 });
 })
 
-publishToCentral("https://6b8ba440e405-onepace-stremio-v2.baby-beamup.club/mainfest.json")
+publishToCentral("https://6b8ba440e405-onepace-stremio-v2.baby-beamup.club/manifest.json")
